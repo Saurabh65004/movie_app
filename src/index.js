307e70c329c82a05e19511f752436a9a32dba4ac
0storecare0@gmail.com <sp65004@gmail.com>
2021-09-01 09:05:21 +0530

    added redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
+import movies from './reducers';
 import App from './components/App';
+
+const store=createStore(movies);
+console.log(store);
+console.log('State',store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +17,15 @@ ReactDOM.render(
 );
 
 
+
+/*
+Created the state and passed our movies reducer in it.
+*/
+
+
+/*
+Our store is just an object with these properties
+{dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, Symbol(observable): ƒ}
+dispatch: ƒ dispatch(action)getState: ƒ getState()replaceReducer: ƒ replaceReducer(nextReducer)subscribe: ƒ subscribe(listener)Symbol(observable): ƒ observable()[[Prototype]]: Object
+
+*/
